21b077138fa5bb4f8b3f1cdd2eca7f18f41e127a
dalmano <dalmano@outlook.com>
2019-10-10 14:54:24 +0100

    Create routes Relates #10

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,7 +1,30 @@
 const router = require('express').Router();
+
+const error = require('./error');
 const home = require('./home');
+const about = require('./about');
+const events = require('./events');
+const currentEvent = require('./events');
+const research = require('./research');
+const downloads = require('./downloads');
+const support = require('./support');
+const volunteer = require('./volunteer');
+const volunteerForm = require('./volunteerForm');
+const bookingForm = require('./bookingForm');
 
 router.get('/', home.get);
+router.get('/about', about.get);
+router.get('/events', events.get);
+router.get('/current-events', currentEvent.get);
+router.get('/research', research.get);
+router.get('/downloads', downloads.get);
+router.get('/support', support.get);
+router.get('/contact', contact.get);
+router.get('/volunteer', volunteer.get);
+router.post('/volunteer-form', volunteerForm.post);
+router.post('/booking-form', bookingForm.post);
 
+router.use(error.client);
+router.use(error.server);
 
 module.exports = router;
