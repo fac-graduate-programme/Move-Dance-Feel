4d3f7e2e78c9cc744d07c3c9ec7b43c9d555ed43
Nareman <naremanmohhilles@gmail.com>
2019-10-17 02:07:04 +0300

    remove input value function and popup show sucsses

--- public/js/domBoking.js
+++ public/js/domBoking.js
@@ -1,5 +1,14 @@
 const formContactUs = document.querySelector('.booking--contactUs-form');
-
+const inputsContactUs = document.querySelectorAll('.booking-inputs');
+const textarea = document.querySelector('.contactUs--formMessage');
+const popup = document.querySelector('.popup');
+const popupMassage = document.querySelector('.popup--content-message');
+const poppUpDone = document.querySelector('.popup--content-Done');
+const innerTextRemove = arrayOfElements => arrayOfElements.forEach((element) => {
+  // eslint-disable-next-line no-param-reassign
+  textarea.value = '';
+  element.value = '';
+});
 formContactUs.addEventListener('submit', (e) => {
   e.preventDefault();
   const formData = new FormData(formContactUs);
@@ -13,5 +22,22 @@ formContactUs.addEventListener('submit', (e) => {
     body: JSON.stringify(dataContactUs),
     headers: { 'Content-Type': 'application/json' },
   })
+    .then(res => res.json())
+    .then((res) => {
+      if (res.msg === 'done') {
+        popup.classList.add('popup-show');
+        popupMassage.textContent = 'We received your message, we will send you our response soon.';
+      }
+    })
+    .catch(() => {
+      popupMassage.textContent = 'error server please try again later';
+    });
+  return true;
+
+});
+poppUpDone.addEventListener('click', (e) => {
+  e.preventDefault();
+  innerTextRemove(Array.from(inputsContactUs).slice(0, 3));
+  popup.classList.remove('popup-show');
 
 });
