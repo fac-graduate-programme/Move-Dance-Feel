286249b0f70f32147313d154b58238d02385c840
Nareman <naremanmohhilles@gmail.com>
2019-10-18 15:50:34 +0300

    add show function to move form suitble to selection

--- public/js/domBoking.js
+++ public/js/domBoking.js
@@ -1,5 +1,13 @@
-const formContactUs = document.querySelector('.booking--contactUs-form');
+const formContactUs = document.querySelector('.contact--contactUs-form');
+function show2() {
+  document.getElementById('booking').style.display = 'block';
+  document.getElementById('Volunteer').style.display = 'none';
 
+}
+function show1() {
+  document.getElementById('booking').style.display = 'none';
+  document.getElementById('Volunteer').style.display = 'block';
+}
 formContactUs.addEventListener('submit', (e) => {
   e.preventDefault();
   const formData = new FormData(formContactUs);
@@ -13,5 +21,4 @@ formContactUs.addEventListener('submit', (e) => {
     body: JSON.stringify(dataContactUs),
     headers: { 'Content-Type': 'application/json' },
   })
-
 });
