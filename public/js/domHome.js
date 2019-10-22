598f356af400ca882aec2412709224616b9aedbb
dubhcait <dubhcait@live.ie>
2019-10-22 18:53:46 +0100

    edit

--- public/js/domHome.js
+++ public/js/domHome.js
@@ -1,7 +1,7 @@
 document.onscroll = () => {
   const nav = document.querySelector('#navbar');
   console.log("test", this.scrollY)
-  if(this.scrollY <= 800) nav.style.backgroundColor = "rgba(255, 255, 255, 0.7)"; else nav.style.backgroundColor = "#fff"; 
+  if(this.scrollY <= 800) {nav.style.backgroundColor = "rgba(255, 255, 255, 0.0)"; nav.style.boxShadow = "none"}else {nav.style.backgroundColor = "#fff"; nav.style.boxShadow = "0px 2px 2px rgba(0, 0, 0, 0.2)"};
 };
 
 // Start of slider code
