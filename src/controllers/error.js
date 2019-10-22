44d2e35ca6edde032b5044e258c620cd0c3ca063
dubhcait <dubhcait@live.ie>
2019-10-22 10:36:56 +0100

    add generic route

--- src/controllers/error.js
+++ src/controllers/error.js
@@ -1,9 +1,4 @@
-exports.client = (req, res) => {
-  res.render('error', {
-    status: 404,
-    errorMessage: 'Page Not Found',
-  });
-};
+
 // eslint-disable-next-line no-unused-vars
 exports.server = (err, req, res, next) => {
   res.render('error', {
---
+++ src/controllers/generic.js
@@ -0,0 +1,32 @@
+const endpoints = [
+  'downloads',
+  'events',
+  'research',
+  'support',
+];
+
+const seo = [
+  'Move Dance Feel UK: information, flyer, download',
+  'Move Dance Feel UK: Support us, be a partner',
+  'Move Dance Feel UK: Research, reports & evidence',
+  'Move Dance Feel UK: Support us, be a partner',
+];
+
+exports.get = (req, res) => {
+  const { endpoint } = req.params;
+  const position = endpoints.indexOf(endpoint);
+
+  if (position !== -1) {
+    const capitalisedEndpoint = endpoint.charAt(0).toUpperCase() + endpoint.slice(1);
+    res.render(endpoint, {
+      js: [`dom${capitalisedEndpoint}`],
+      css: endpoint,
+      title: seo[position],
+    });
+  } else {
+    res.render('error', {
+      status: 404,
+      errorMessage: 'Page Not Found',
+    });
+  }
+};
